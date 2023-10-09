b583dda2c996614c487d528d1e76caaaf558b468
ThIsLinked <kolyalitvintsev@gmail.com>
2023-10-10 01:25:46 +0300

    Optimization code.

--- app/src/main/java/ru/fourpda/skins/BindReceiver.java
+++ app/src/main/java/ru/fourpda/skins/BindReceiver.java
@@ -4,10 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-public class BindReceiver extends BroadcastReceiver
-{
+public class BindReceiver extends BroadcastReceiver {
     @Override
-    public void onReceive(Context context, Intent intent)
-    {
+    public void onReceive(Context context, Intent intent) {
     }
 }
