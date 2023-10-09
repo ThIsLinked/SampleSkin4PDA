7fa45f0e138cc53f90be0be44471fb3b9d361bbd
ThIsLinked <kolyalitvintsev@gmail.com>
2023-10-10 00:49:17 +0300

    Convert to new syntax.

---
+++ app/build.gradle.kts
@@ -0,0 +1,73 @@
+/*
+ * Aurora Store
+ *  Copyright (C) 2021, Rahul Kumar Patel <whyorean@gmail.com>
+ *  Copyright (C) 2022, The Calyx Institute
+ *  Copyright (C) 2023, grrfe <grrfe@420blaze.it>
+ *
+ *  Aurora Store is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Aurora Store is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Aurora Store.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+import java.util.Properties
+
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "ru.fourpda.skins.sample"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "ru.fourpda.skins.sample"
+        minSdk = 10
+        targetSdk = 28
+
+        versionCode = 10941
+        versionName = "@string/app_versionName"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            isShrinkResources = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        buildConfig = true /* Enable generate BuildConfig */
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    packaging {
+        jniLibs.useLegacyPackaging = false
+    }
+
+    buildToolsVersion = "34.0.0"
+    ndkVersion = "26.0.10792818"
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    testImplementation("junit:junit:4.13.2")
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
@@ -6,10 +7,10 @@ buildscript {
         google()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:8.0.2'
+        classpath("com.android.tools.build:gradle:8.1.2")
 
         // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
+        // in the individual module build.gradle.kts files
     }
 }
 
@@ -20,6 +21,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
